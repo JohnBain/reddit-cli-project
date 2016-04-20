2ce0f6f1fe77f685a36b243586ccfe563631362c
jbain1 <jbainschool@gmail.com>
2016-04-20 18:53:46 +0000

    Wrote Reddit module

--- reddit.js
+++ reddit.js
@@ -1,45 +1,111 @@
 var request = require('request');
+var util = require("util");
 
 /*
 This function should "return" the default homepage posts as an array of objects
 */
+
 function getHomepage(callback) {
-  // Load reddit.com/.json and call back with the array of posts
+  request("https://www.reddit.com/.json", function(err, result) {
+    var redditHomepage = JSON.parse(result.body);
+    callback(redditHomepage.data.children);
+  });
 }
+
+/*
+getHomepage(function(res){
+  console.log(util.inspect(res, { showHidden: true, depth: null, colors: true }));
+});
+*/
 
 /*
 This function should "return" the default homepage posts as an array of objects.
 In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.
 */
+
+
 function getSortedHomepage(sortingMethod, callback) {
-  // Load reddit.com/{sortingMethod}.json and call back with the array of posts
-  // Check if the sorting method is valid based on the various Reddit sorting methods
+  var baseReddit = "https://www.reddit.com/";
+  baseReddit += sortingMethod + "/.json"
+
+  request(baseReddit, function(err, result) {
+    var redditHomepage = JSON.parse(result.body);
+    callback(redditHomepage.data.children);
+  });
+
 }
+
+/*
+getSortedHomepage("gilded", function(res){
+  console.log(util.inspect(res, { showHidden: true, depth: null, colors: true }));
+});
+*/
+
 
 /*
 This function should "return" the posts on the front page of a subreddit as an array of objects.
 */
 function getSubreddit(subreddit, callback) {
   // Load reddit.com/r/{subreddit}.json and call back with the array of posts
+  var baseReddit = "https://www.reddit.com/r/"
+  baseReddit += subreddit += "/.json"
+  console.log(baseReddit)
+  request(baseReddit, function(err, result) {
+        var redditHomepage = JSON.parse(result.body);
+        callback(redditHomepage.data.children);
+  });
 }
+/*
+getSubreddit("okcupid", function(res){
+  console.log(util.inspect(res, { showHidden: true, depth: null, colors: true }));
+}); */
+
 
 /*
 This function should "return" the posts on the front page of a subreddit as an array of objects.
 In contrast to the `getSubreddit` function, this one accepts a `sortingMethod` parameter.
 */
+
 function getSortedSubreddit(subreddit, sortingMethod, callback) {
+    var baseReddit = "https://www.reddit.com/r/"
+    baseReddit += subreddit + "/" + sortingMethod + "/.json";
+    console.log(baseReddit);
+    request(baseReddit, function(err, result) {
+        var redditHomepage = JSON.parse(result.body);
+        callback(redditHomepage.data.children);
+  });
+}
+/*
+getSortedSubreddit("okcupid", "gilded", function(res){
+  console.log(util.inspect(res, { showHidden: true, depth: null, colors: true }));
+})
+
+*/
   // Load reddit.com/r/{subreddit}/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
-}
+
 
 /*
 This function should "return" all the popular subreddits
 */
 function getSubreddits(callback) {
+  var baseReddit = "https://www.reddit.com/subreddits.json";
+    request(baseReddit, function(err, result) {
+        var redditHomepage = JSON.parse(result.body);
+        callback(redditHomepage.data.children);
+  });
   // Load reddit.com/subreddits.json and call back with an array of subreddits
 }
 
+getSubreddits(function(res){
+  console.log(util.inspect(res, { showHidden: true, depth: null, colors: true }));
+})
+
 // Export the API
 module.exports = {
-  // ...
+  getHomepage : getHomepage,
+  getSortedHomepage : getSortedHomepage,
+  getSubreddit : getSubreddit,
+  getSortedSubreddit : getSortedSubreddit,
+  getSubreddits : getSubreddits
 };
